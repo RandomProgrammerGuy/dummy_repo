23bd7944a623983226b8279ac6b1d741369d554a
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2022-09-22 22:05:43 +0200

    Added confirm send and sending method

--- mailman/mailman.py
+++ mailman/mailman.py
@@ -12,3 +12,12 @@ to = input()
 
 print('\n Message: ')
 message = input()
+
+# -- Confirm send and send method -- #
+print('\n You want to send the message "' + message + '" to ' + to + ". Is that correct? \n Press Y to confirm, N to deny")
+confirm = input()
+if confirm == 'y' or confirm == 'Y':
+    server.sendmail('farjamautomatedsender@gmail.com', to , message)
+    print("Mail has been (hopefully) sent")
+else:
+    print('Mail send declined')
