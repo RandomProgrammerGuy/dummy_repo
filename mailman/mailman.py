b19211d4a427f3ea7e6b5701dc0f97b508db0c1b
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2024-07-06 14:27:06 +0200

    Updated data to match locally stored data and added new stuff

---
+++ mailman/mailman.py
@@ -0,0 +1,25 @@
+import smtplib
+
+# -- Define SMTP Server -- #
+server = smtplib.SMTP('smtp.gmail.com', 587)
+
+# -- Login to Gmail account with SMTP -- #
+server.login('farjamautomatedsender@gmail.com', 'VerySecurePassword!')
+
+server.starttls()
+
+# -- Recieve destination and message -- #
+print('Send to: ')
+to = input()
+
+print('\n Message: ')
+message = input()
+
+# -- Confirm send and send method -- #
+print('\n You want to send the message "' + message + '" to ' + to + ". Is that correct? \n Press Y to confirm, N to deny")
+confirm = input()
+if confirm == 'y' or confirm == 'Y':
+    server.sendmail('farjamautomatedsender@gmail.com', to , message)
+    print("Mail has been (hopefully) sent")
+else:
+    print('Mail send declined')
