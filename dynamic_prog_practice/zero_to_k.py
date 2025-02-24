e6a2bad098cd9e91292ca4faf34c787cb942a560
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2025-02-24 13:21:26 +0100

    Added docs for zero to k

---
+++ dynamic_prog_practice/zero_to_k.py
@@ -0,0 +1,10 @@
+# [   CODE BY @RandomProgrammerGuy ON GITHUB   ]------------------------------------- #
+
+# [   PROBLEM DESCRIPTION   ]-------------------------------------------------------- #
+# Given a positive integer K, the task is to find the minimum number of operations of #
+# the following two types, required to change 0 to K:                                 #
+# - Add one to the operand                                                            #
+# - Multiply the operand by 2                                                         #
+# ----------------------------------------------------------------------------------- #
+
+
