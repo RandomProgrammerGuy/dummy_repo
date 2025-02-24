b7b7f1091cdd4932b020bf575087b12f7f9d654c
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2025-02-24 14:27:18 +0100

    Added docs for minimum_coins

---
+++ dynamic_prog_practice/minimum_coins.py
@@ -0,0 +1,8 @@
+# [   CODE BY @RandomProgrammerGuy ON GITHUB   ]------------------------------------- #
+
+# [   PROBLEM DESCRIPTION   ]-------------------------------------------------------- #
+# Given an array of coins[] of size n and a target value sum, where coins[i]          #
+# represent the coins of different denominations. You have an infinite supply of each #
+# of the coins. The task is to find the minimum number of coins required to make the  #
+# given value sum. If it’s not possible to make a change, return -1.                  #
+# ----------------------------------------------------------------------------------- #
