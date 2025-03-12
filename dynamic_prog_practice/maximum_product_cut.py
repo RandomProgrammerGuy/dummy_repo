a2b234fa71c7a0a73fc8bca7b58ea4e52213cc66
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2025-03-12 16:09:46 +0100

    Added solution for maximum_product_cut

--- dynamic_prog_practice/maximum_product_cut.py
+++ dynamic_prog_practice/maximum_product_cut.py
@@ -10,9 +10,41 @@ from random import randint
 from time import time
 
 # Initializing the cord length variable
-cord_length = randint(2,20)
+cord_length = randint(2,100)
 
 print(f'Randomly generated cord length = {cord_length}')
 
 # Algorithm starts here
 start_time = time()
+
+# N+1 x N array where each line represents cord length and each column the number of cuts
+lengths_cuts = [ [0 for i in range(cord_length)] for j in range(cord_length+1) ]
+
+
+# Initializing value for cord length 2 (minimum)
+lengths_cuts[2][1] = 1
+
+for i in range(3, cord_length + 1):
+
+    for j in range(1, cord_length):
+        if j == 1:
+            lengths_cuts[i][j] = (i // 2) * (i - (i // 2))
+        
+        elif j < i :
+            maximum = 1
+
+            for k in range(1, i - (j - 1) + 1):
+                if k * lengths_cuts[i - k][j - 1] > maximum:
+                    maximum = k * lengths_cuts[i - k][j - 1]
+
+            lengths_cuts[i][j] = maximum
+
+        else:
+            break
+
+result = max(lengths_cuts[i])
+end_time = time()
+
+# Printing results
+print(f'Optimal cut product for cord length = {cord_length} is {result}')
+print(f'Elapsed time = {end_time - start_time}s')
